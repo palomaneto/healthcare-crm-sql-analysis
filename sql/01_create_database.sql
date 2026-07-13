f181eeb1c5f5e53a9e45eb337819224cfd4684c3
Palz <palomamavacala@hotmail.com>
2026-07-13 09:24:40 +0100

    Create 01_create_database.sql
    Create healthcare CRM database

---
+++ sql/01_create_database.sql
@@ -0,0 +1,14 @@
+/*
+=========================================================
+Healthcare CRM SQL Analysis
+File: 01_create_database.sql
+
+Objective:
+Create a separate MySQL database for analysing patient
+engagement, retention and healthcare CRM activity.
+=========================================================
+*/
+
+CREATE DATABASE healthcare_crm_analysis;
+
+USE healthcare_crm_analysis;
